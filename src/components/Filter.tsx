c1393a4de49b0173a5cd2520b1f53b3059bedf21
Carlos Benítez <cb2724136@gmail.com>
2024-09-20 18:54:20 -0600

    feat: migrate styles from Filters.css to Tailwind CSS(some rules pending)

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -9,7 +9,7 @@ type Props = {
 function Filter({ filterName, filterColor, onClick }: Props) {
   return (
     <div
-      className={`filter ${filterColor} shadow-[0_0.4rem_1.5rem_rgba(0,0,0,0.219)] bg-[#ececec]  dark:text-[#0e1217]`}
+      className={`cursor-pointer rounded-full px-6 py-3 font-medium transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ${filterColor} bg-[#ececec] shadow-[0_0.4rem_1.5rem_rgba(0,0,0,0.219)] dark:text-[#0e1217]`}
       onClick={() => onClick(filterName)}
     >
       <p className="filter-text">{filterName.split("_")[0].toUpperCase()}</p>
